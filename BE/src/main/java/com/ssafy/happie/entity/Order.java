14bc0fb70bcfeac0edc33741ba1dfa3d7a35fe5a
손채이 <im_1998@naver.com>
2025-03-31 17:07:28 +0900

    chore: DB 세팅

--- BE/src/main/java/com/ssafy/happie/entity/Order.java
+++ BE/src/main/java/com/ssafy/happie/entity/Order.java
@@ -1,4 +1,22 @@
 package com.ssafy.happie.entity;
 
+import jakarta.persistence.*;
+
+@Entity(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String robot;
+
+    @Column
+    private String place;
+
+    @Column
+    private String todo;
+
+    @Column
+    private String state;
 }
