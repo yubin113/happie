3c16344017322cf352ee3305131c9638c5b2fe87
김태영 <teayoung0812@naver.com>
2025-04-02 17:41:58 +0900

    feat: 맵 파라미터 수정

--- ROS/auto_driving/happie/happie/config.py
+++ ROS/auto_driving/happie/happie/config.py
@@ -1,12 +1,12 @@
 # # custom map parameters
 params_map = {
-    "MAP_RESOLUTION": 0.1,
+    "MAP_RESOLUTION": 0.03,
     "OCCUPANCY_UP": 0.02,
     "OCCUPANCY_DOWN": 0.01,
     "MAP_CENTER": (-50, -50),
     "MAP_SIZE": (30, 30),
     "MAP_FILENAME": 'test.png',
-    "MAPVIS_RESIZE_SCALE": 2.5
+    "MAPVIS_RESIZE_SCALE": 0.5
 }
 
 # smart home 1 map parameters
