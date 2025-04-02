08c26ca3d8a4acbf8c554102672748140b7095ef
김태영 <teayoung0812@naver.com>
2025-04-02 17:46:00 +0900

    atar 알고리즘 수정

--- ROS/auto_driving/happie/happie/run_mapping.py
+++ ROS/auto_driving/happie/happie/run_mapping.py
@@ -9,6 +9,7 @@ from squaternion import Quaternion
 from nav_msgs.msg import Odometry, Path, OccupancyGrid, MapMetaData
 from math import pi, cos, sin, sqrt
 import tf2_ros
+import heapq
 import os
 import happie.utils as utils
 import numpy as np
@@ -17,6 +18,8 @@ import time
 
 from .config import params_map, PKG_PATH, MQTT_CONFIG
 import paho.mqtt.client as mqtt
+
+import matplotlib.pyplot as plt
 
 # mapping node의 전체 로직 순서
 # 1. publisher, subscriber, msg 생성
@@ -76,6 +79,30 @@ def createLineIterator(P1, P2, img):
     itbuffer = itbuffer[(itbuffer[:, 0] >= 0) & (itbuffer[:, 0] < imageW) & (itbuffer[:, 1] >= 0) & (itbuffer[:, 1] < imageH)]
     return itbuffer
 
+# A* 경로를 ROS Path 메시지로 변환하는 함수
+def convert_path_to_ros(path, map_center, map_resolution):
+    ros_path = Path()
+    ros_path.header.frame_id = "map"  # TF 좌표계 설정
+    ros_path.header.stamp = rclpy.clock.Clock().now().to_msg()
+
+    for node in path:
+        i, j = node  # A*에서의 (grid y, grid x) 좌표
+
+        # Grid 좌표 → 실제 좌표 변환
+        real_x = map_center[0] + (j * map_resolution)
+        real_y = map_center[1] + (i * map_resolution)
+
+        # PoseStamped 메시지 생성
+        pose = PoseStamped()
+        pose.header.frame_id = "map"
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = real_x
+        pose.pose.position.y = real_y
+        pose.pose.position.z = 0.0  # 2D 경로이므로 z=0
+
+        ros_path.poses.append(pose)
+
+    return ros_path
 
 class Mapping:
     # 사용자가 정의한 맵 설정을 받아서 회색의 어레이로 초기화 시키고,
@@ -155,11 +182,11 @@ class Mapping:
             avail_y = line_iter[:, 1].astype(np.int32)
 
             ## Empty
-            self.map[avail_y[:-1], avail_x[:-1]] += self.occu_down
+            self.map[avail_y[:-1], avail_x[:-1]] -= self.occu_down
             self.map[avail_y[:-1], avail_x[:-1]] = np.clip(self.map[avail_y[:-1], avail_x[:-1]], 0, 1)
 
             ## Occupied
-            self.map[avail_y[-1], avail_x[-1]] -= self.occu_up
+            self.map[avail_y[-1], avail_x[-1]] += self.occu_up
             self.map[avail_y[-1], avail_x[-1]] = np.clip(self.map[avail_y[-1], avail_x[-1]], 0, 1)
                 
         self.show_pose_and_points(pose, laser_global) 
@@ -212,6 +239,8 @@ class Mapper(Node):
         
         # 로직 1 : publisher, subscriber, msg 생성
         self.subscription = self.create_subscription(LaserScan,'/scan',self.scan_callback,10)
+        self.goal_sub = self.create_subscription(PoseStamped, 'goal_pose', self.goal_callback, 1)
+        self.global_path_pub = self.create_publisher(Path, 'a_star_global_path', 10)
 
         # MQTT 설정
         self.mqtt_client = mqtt.Client()
@@ -232,6 +261,12 @@ class Mapper(Node):
         self.map_size=int(params_map["MAP_SIZE"][0]\
             /params_map["MAP_RESOLUTION"]*params_map["MAP_SIZE"][1]/params_map["MAP_RESOLUTION"])
         
+        self.grid = []
+        self.rows = []
+        self.cols = []
+                
+        self.map_pose_x = 0
+        self.map_pose_y = 0
 
         m = MapMetaData()
         m.resolution = params_map["MAP_RESOLUTION"]
@@ -253,17 +288,128 @@ class Mapper(Node):
         # 로직 2 : mapping 클래스 생성
         self.mapping = Mapping(params_map)
 
-    
-    def scan_callback(self, msg):
-        print("scan_callback start!!!")
-        # 로직 4 : laser scan 메시지 안의 ground truth pose 받기
-        pose_x = msg.range_min
-        pose_y = msg.scan_time
-        heading = msg.time_increment
-        
-        # 로직 5 : lidar scan 결과 수신
-        distance = np.array(msg.ranges)  # LaserScan의 거리 데이터 (길이 360의 배열)
+    def heuristic(self, a, b):
+        # 맨해튼 거리 (거리 계산 방법을 변경할 수 있음)
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
+    def neighbors(self, node):
+        directions = [
+            (-1, 0), (1, 0), (0, -1), (0, 1),  # 상, 하, 좌, 우
+            (-1, -1), (-1, 1), (1, -1), (1, 1) # 대각선
+        ]
+        dCost = [1, 1, 1, 1, 1.414, 1.414, 1.414, 1.414] # 이동 비용 설정
+        neighbors = []
+        # 경계를 벗어나지 않고 벽(40 이상)이 아니면 유효한 인접 노드
+        for i, direction in enumerate(directions):
+            neighbor = (node[0] + direction[0], node[1] + direction[1])
+            if 0 <= neighbor[0] < self.rows and 0 <= neighbor[1] < self.cols and self.grid[neighbor[0]][neighbor[1]] < 40:
+                neighbors.append((neighbor, dCost[i]))
+        return neighbors
+
+    def a_star(self, start, goal):
+
+        def grid_to_real(path, params):
+            grid_size = int(params["MAP_SIZE"][0] / params["MAP_RESOLUTION"])  # 그리드 크기 계산
+            x_center, y_center = params["MAP_CENTER"]  # 맵 중심 좌표
+            resolution = params["MAP_RESOLUTION"]  # 해상도
+
+            real_path = [
+                (
+                    x_center + (j - grid_size // 2) * resolution,
+                    y_center + (i - grid_size // 2) * resolution
+                )
+                for i, j in path
+            ]
+            return real_path
+        
+        open_list = []
+        closed_list = set()
+        
+        heapq.heappush(open_list, (0 + self.heuristic(start, goal), 0, self.heuristic(start, goal), start))
+        
+        came_from = {}
+        g_score = {start: 0}
+        
+        while open_list:
+            current_f, current_g, current_h, current_node = heapq.heappop(open_list)
+            
+            if current_node == goal:
+                path = []
+                while current_node in came_from:
+                    path.append(current_node)
+                    current_node = came_from[current_node]
+                path.append(start)
+                real_path = grid_to_real(path[::-1], params_map)
+                return path[::-1], real_path
+            
+            closed_list.add(current_node)
+            
+            for neighbor, cost in self.neighbors(current_node):
+                if neighbor in closed_list:
+                    continue
+                
+                tentative_g_score = current_g + cost  
+                
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    g_score[neighbor] = tentative_g_score
+                    f_score = tentative_g_score + self.heuristic(neighbor, goal)
+                    heapq.heappush(open_list, (f_score, tentative_g_score, self.heuristic(neighbor, goal), neighbor))
+                    came_from[neighbor] = current_node
+
+        return None
+
+    def publish_global_path(self, path_points):
+        """경로를 Path 메시지로 변환 후 Publish"""
+        path_msg = Path()
+        path_msg.header.stamp = self.get_clock().now().to_msg()
+        path_msg.header.frame_id = "map"
+
+        for x, y in path_points:
+            pose = PoseStamped()
+            pose.header.stamp = self.get_clock().now().to_msg()
+            pose.header.frame_id = "map"
+            pose.pose.position.x = x
+            pose.pose.position.y = y
+            pose.pose.orientation.w = 1.0  # 기본 방향 설정
+            path_msg.poses.append(pose)
+
+        self.global_path_pub.publish(path_msg)
+        self.get_logger().info("Published global path.")
+
+
+    def scan_callback(self, msg):
+        # print("scan_callback start!!!")
+    
+        # [1] 현재 위치 (pose_x, pose_y, heading) 가져오기
+        pose_x = msg.range_min  # 실제 x 좌표 (meters)
+        pose_y = msg.scan_time  # 실제 y 좌표 (meters)
+        heading = msg.time_increment  # 로봇의 방향 (radians)
+        
+        # [2] 거리 데이터를 기반으로 LIDAR 스캔 변환
+        distance = np.array(msg.ranges)
+        angles = np.linspace(0, 2 * np.pi, len(distance), endpoint=False)
+        x = distance * np.cos(angles)
+        y = distance * np.sin(angles)
+        laser = np.vstack((x, y))  
+    
+        # [3] 현재 위치를 Grid Map 좌표계로 변환
+        MAP_RESOLUTION = params_map["MAP_RESOLUTION"]
+        MAP_CENTER = params_map["MAP_CENTER"]
+        MAP_SIZE = params_map["MAP_SIZE"]
+    
+        map_x = (pose_x - MAP_CENTER[0] + MAP_SIZE[0]/2) / MAP_RESOLUTION
+        map_y = (pose_y - MAP_CENTER[1] + MAP_SIZE[1]/2) / MAP_RESOLUTION
+        self.map_pose_x = map_x
+        self.map_pose_y = map_y
+
+        # pose = np.array([[pose_x], [pose_y], [heading]])
+        # self.mapping.update(pose, laser)
+    
+        # [4] 로그 출력 (현재 위치 확인)
+        print(f"현재 위치 (실제 좌표): x={pose_x:.2f}, y={pose_y:.2f}, heading={heading:.2f} rad")
+        print(f"맵 좌표계 인덱스: map_x={map_x:.0f}, map_y={map_y:.0f}")
+    
+        # [5] 맵 퍼블리시
         # 각도 계산 (1도씩 증가하므로, 각도를 라디안으로 변환)
         angles = np.linspace(0, 2 * np.pi, len(distance), endpoint=False)  # 360개의 각도 생성 (0에서 2π까지)
 
@@ -296,18 +442,81 @@ class Mapper(Node):
         np_map_data = self.mapping.map.reshape(-1)
         list_map_data = [100 - int(value * 100) for value in np_map_data]
         list_map_data = [max(0, min(100, v)) for v in list_map_data]
-
-        # 로직 11 : 업데이트 중인 map publish
-
-        self.map_msg.header.stamp =rclpy.clock.Clock().now().to_msg()
-        # self.map_msg.data = (self.mapping.map.flatten() * 100).astype(np.int32).tolist()
+    
+        self.map_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
         self.map_msg.data = np.clip((self.mapping.map.flatten() * 100), -128, 127).astype(np.int32).tolist()
         self.map_pub.publish(self.map_msg)
-
+    
+        # [6] 10초마다 맵 저장
         current_time = time.time()
-        if current_time - self.last_save_time > 10:  # 10초마다 저장
+        if current_time - self.last_save_time > 10:
             save_map(self, 'map.txt')
             self.last_save_time = current_time
+
+    def goal_callback(self, msg):
+        if msg.header.frame_id == 'map':
+            goal_x = msg.pose.position.x
+            goal_y = msg.pose.position.y
+            print(f"목표 위치 (실제 좌표): x={goal_x:.2f}, y={goal_y:.2f}")
+            # 위치변환
+            goal_map_x = (goal_x - params_map['MAP_CENTER'][0] + params_map['MAP_SIZE'][0]/2) / params_map['MAP_RESOLUTION']
+            goal_map_y = (goal_y - params_map['MAP_CENTER'][1] + params_map['MAP_SIZE'][1]/2) / params_map['MAP_RESOLUTION']
+            # 목표 위치 확인
+            print(f"맵 좌표계 인덱스: map_x={goal_map_x:.0f}, map_y={goal_map_y:.0f}")
+
+
+            # 파일 경로 설정
+            back_folder = '..'  # 상위 폴더를 지정하려는 경우
+            PKG_PATH = r'C:\Users\SSAFY\Desktop\S12P21E103\ROS\auto_driving\happie\happie'
+            folder_name = 'data'  # 맵을 저장할 폴더 이름
+            file_name = 'map.txt'  # 파일 이름
+            full_path = os.path.join(PKG_PATH, back_folder, folder_name, file_name)  # 전체 경로 설정
+
+            # 데이터 읽기
+            with open(full_path, 'r') as file:
+                data = file.read().split()
+    
+            # 데이터 크기 확인
+            grid_size = int(params_map['MAP_SIZE'][0]/params_map['MAP_RESOLUTION'])
+
+            # 1차원 배열을 NxM 크기의 2차원 배열로 변환
+            data_array = np.array(data, dtype=int).reshape(grid_size, grid_size)
+            self.grid = data_array
+            self.rows = len(self.grid)
+            self.cols = len((self.grid)[0])
+            start = (int(self.map_pose_y), int(self.map_pose_x))
+            goal = (int(590.0), int(756.0))
+            # goal = (int(132.0), int(188.0))
+            path, real_path = self.a_star(start, goal)
+            print(real_path)
+            # 경로 표시
+            if path:
+                # 경로를 맵에 빨간색으로 표시
+                for p in path:
+                    data_array[p[0]][p[1]] = 50  # 경로 표시 (예: 값 50으로 표시)
+
+            # 경로가 제대로 표시되지 않으면 경로를 점으로만 표시
+            else:
+                print("경로를 찾을 수 없습니다.")
+            
+            # 만든 path를 publish
+            self.publish_global_path(real_path)
+
+            # 시각화 (matplotlib 사용)
+            fig, ax = plt.subplots()
+            # 먼저 전체 맵을 그립니다
+            cax = ax.imshow(data_array, cmap='gray', interpolation='nearest')
+            # 경로를 빨간색으로 그립니다
+            if path:
+                for p in path:
+                    ax.plot(p[1], p[0], color='red', marker='o', markersize=2)  # 경로를 빨간색 점으로 표시
+
+            plt.colorbar(cax)  # 색상 막대 추가
+            plt.title("A* Pathfinding with Red Path")
+            plt.show()
+
+
+
 
 
 def save_map(node, file_path):
@@ -321,23 +530,13 @@ def save_map(node, file_path):
     full_path=os.path.join(pkg_path,back_folder,folder_name,file_name)
     print(full_path)
     
-    f=open(full_path,'w')
+    f=open(full_path,'w+')
     data=''
     for pixel in node.map_msg.data :
         data+='{0} '.format(pixel)
     print("map 데이터 저장 완료")
     f.write(data) 
     f.close()
-
-    # # node.map_msg.data가 1D 배열이므로 2D 배열로 변환 (예: 맵 크기 지정)
-    # map_width = int(params_map['MAP_SIZE'][0]/params_map['MAP_RESOLUTION'])
-    # map_height = int(params_map['MAP_SIZE'][1]/params_map['MAP_RESOLUTION'])
-    
-    # # 1D 데이터를 2D 배열로 변환
-    # map_data = np.array(node.map_msg.data).reshape(int(map_height), int(map_width))
-
-    # # 회전된 맵을 1D 배열로 다시 변환
-    # map_data_flat = map_data.flatten()
 
 
 def main(args=None):    
